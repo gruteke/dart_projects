8e273923f0b6224749498dbf80d82de2d908e8be
Rudi Gruteke <rgruteke@yahoo.com>
2014-04-13 17:22:23 +0300

    verb something

--- s1_basics/lib/badge_controller.dart
+++ s1_basics/lib/badge_controller.dart
@@ -3,23 +3,46 @@ library s1_basics.badge_controller;
 import 'package:angular/angular.dart';
 import 'dart:math' show Random;
 import 'package:s1_basics/model.dart';
+import 'dart:async' show Future;
 
 
 @NgController(
   selector: '[badge-controller]',
   publishAs: 'ctrl')
 class BadgeController {
+  final Http _http;
+  bool dataLoaded = false;
+  
+  Future _loadData() {
+      return _http.get('packages/s1_basics/assets/piratenames.json')
+          .then((response) {
+            names = response.data['names'];
+            appellations = response.data['appellations'];
+          });
+    }
+  
+  BadgeController(this._http) {
+      _loadData()
+          .then((_) => dataLoaded = true)
+          .catchError((error) {
+            print('Could not read data from the JSON file: $error');
+          });
+    }
+  
   //static const DEFAULT_NAME = 'Anne Bonney';
     static const LABEL1 = 'Arrr! Write yer name!';
     static const LABEL2 = 'Aye! Gimme a name!';
     
-    static const List names = const [
-       'Anne', 'Mary', 'Jack', 'Morgan', 'Roger',
-       'Bill', 'Ragnar', 'Ed', 'John', 'Jane' ];
+    //static const List names = const [
+     //  'Anne', 'Mary', 'Jack', 'Morgan', 'Roger',
+     //  'Bill', 'Ragnar', 'Ed', 'John', 'Jane' ];
 
-     static const List appellations = const [
-       'Jackal', 'King', 'Red', 'Stalwart', 'Axe',
-       'Young', 'Brave', 'Eager', 'Wily', 'Zesty'];
+    // static const List appellations = const [
+     //  'Jackal', 'King', 'Red', 'Stalwart', 'Axe',
+     //  'Young', 'Brave', 'Eager', 'Wily', 'Zesty'];
+     
+     static List<String> names = [];
+     static List<String> appellations = [];
     
      String _name = '';
 
