70c0708c7613f539fb886b5610c3ed860688be0a
Rudi Gruteke <rgruteke@yahoo.com>
2014-04-14 08:59:48 +0300

    add filters/capitalize_filter.dart

--- s1_basics/lib/pirate_module.dart
+++ s1_basics/lib/pirate_module.dart
@@ -4,11 +4,14 @@ import 'package:angular/angular.dart';
 import 'package:s1_basics/badge_controller.dart'; 
 import 'package:s1_basics/components/badge_component.dart';
 import 'package:s1_basics/services/names_service.dart';
+import 'package:s1_basics/filters/capitalize_filter.dart';
 
 class PirateModule extends Module {
   PirateModule() {
     type(BadgeController);
     type(BadgeComponent);
     type(NamesService);
+    type(CapitalizeFilter);
+   
   }
 }
